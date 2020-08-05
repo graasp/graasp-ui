849258534c114925367c687a19358312c243222d
juancarlosfarah <juancarlos.farah@pm.me>
2020-08-05 18:52:37 +0200

    chore: update github pages

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
-import { ExampleComponent } from 'graasp-ui'
-import 'graasp-ui/dist/index.css'
+import { ExampleComponent } from '@graasp/ui'
+import '@graasp/ui/dist/index.css'
 
 const App = () => {
   return <ExampleComponent text="Create React Library Example 😄" />
