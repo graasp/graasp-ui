a78af21641a4ae8a840f2b926b2ea04e5edcaf29
pyphilia <pyphilia@gmail.com>
2020-08-26 11:32:20 +0200

    refactor: set up Loader default type to bubbles

--- src/Loader/Loader.js
+++ src/Loader/Loader.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactLoading from 'react-loading'
 import { PRIMARY_COLOR } from '../theme'
 
-const Loader = ({ type, color = PRIMARY_COLOR, className }) => (
+const Loader = ({ type = 'bubbles', color = PRIMARY_COLOR, className }) => (
   <div className={className}>
     <ReactLoading type={type} color={color} />
   </div>
