d8c45686417108e343fad08cd489b0e90901284d
Abdallah <abdallah.alchami1@gmail.com>
2021-06-24 10:47:54 +0200

    feat: add link button for iframe

--- src/items/LinkItem.js
+++ src/items/LinkItem.js
@@ -1,12 +1,30 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core';
+import React, { useState } from 'react';
+import { IconButton, makeStyles } from '@material-ui/core';
 import { getEmbeddedLinkExtra } from '../utils/itemExtra';
+import OpenInNewIcon from '@material-ui/icons/OpenInNew';
 import withCaption from './withCaption';
 
 const useStyles = makeStyles(() => ({
   iframe: {
     width: '100%',
     border: 'none',
+  },
+  linkButtonContainer: {
+    position: 'absolute',
+    bottom: 0,
+    right: 0,
+    width: 50,
+    height: 50,
+    backgroundColor: 'rgba(62,62,62,0.55)',
+  },
+  linkButton: {
+    padding: 2,
+    marginLeft: 'auto',
+    marginRight: 'auto',
+  },
+  linkButtonIcon: {
+    color: 'white',
+    fontSize: 46,
   },
 }));
 
@@ -19,6 +37,8 @@ const LinkItem = ({
   saveButtonId,
 }) => {
   const classes = useStyles();
+
+  const [loaded, setLoaded] = useState(false);
 
   const id = item.get('id');
   const extra = getEmbeddedLinkExtra(item.get('extra'));
@@ -47,14 +67,40 @@ const LinkItem = ({
   const url = extra?.url;
   const name = item.get('name');
 
+  const handleLoad = () => {
+    setLoaded(true);
+  };
+
   const component = (
-    <iframe
-      id={id}
-      className={classes.iframe}
-      title={name}
-      src={url}
-      height={height}
-    />
+    <React.Fragment>
+      <div
+        hidden={loaded}
+        style={{ position: 'relative', height: height || '100%' }}
+      >
+        Link is Loading.
+        <a href={url}> Click here to open link manually.</a>
+      </div>
+      <div
+        hidden={!loaded}
+        style={{ position: 'relative', height: height || '100%' }}
+      >
+        <iframe
+          id={id}
+          className={classes.iframe}
+          title={name}
+          src={url}
+          onLoad={handleLoad}
+          height='100%'
+        />
+        <div className={classes.linkButtonContainer}>
+          <IconButton className={classes.linkButton}>
+            <a href={url} target='_blank' rel='noreferrer'>
+              <OpenInNewIcon className={classes.linkButtonIcon} />
+            </a>
+          </IconButton>
+        </div>
+      </div>
+    </React.Fragment>
   );
 
   if (showCaption) {
