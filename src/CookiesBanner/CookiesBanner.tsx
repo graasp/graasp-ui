d122b66ab521756493fff80b034acbd0857c5e8c
pyphilia <pyphilia@gmail.com>
2022-01-28 17:31:05 +0100

    refactor: apply PR requested changes

--- src/CookiesBanner/CookiesBanner.tsx
+++ src/CookiesBanner/CookiesBanner.tsx
@@ -42,8 +42,8 @@ interface CookiesBannerProps {
 
 const CookiesBanner: FC<CookiesBannerProps> = ({
   cookieName,
-  acceptText = 'Accept all',
-  declineButtonText = 'Reject non-essential',
+  acceptText = 'Accept All',
+  declineButtonText = 'Reject Non-Essential',
   text = `We use cookies and other tracking technologies to improve your browsing experience on our website, to analyze our website traffic, and to understand where our visitors are coming from. By browsing our website, you consent to our use of cookies and other tracking technologies.`,
   expires = 30,
 }) => {
