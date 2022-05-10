42c200404e7ac9a66036c01210ce627e778032f4
alvrba <alvaro.b72@gmail.com>
2022-05-10 16:04:10 +0200

    refactor: use prettier

--- src/Collapse/withCollapse.tsx
+++ src/Collapse/withCollapse.tsx
@@ -7,15 +7,11 @@ interface WithCollapseProps<T> {
   item: Record<Item<T>>;
 }
 
-function withCollapse<T extends UnknownExtra>({
-  item,
-}: WithCollapseProps<T>) {
+function withCollapse<T extends UnknownExtra>({ item }: WithCollapseProps<T>) {
   return (component: JSX.Element): JSX.Element => {
     class ComponentWithCollapse extends React.Component {
       render(): JSX.Element {
-        return (
-            <Collapse title={item.get('name')} content={component} />
-        );
+        return <Collapse title={item.get('name')} content={component} />;
       }
     }
 
