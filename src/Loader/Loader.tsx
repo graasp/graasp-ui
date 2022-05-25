12cd636c058194dff723692667b96913a955e248
spaenleh <basile.1358@gmail.com>
2022-05-25 15:56:03 +0200

    feat: add sx prop to Loader

--- src/Loader/Loader.tsx
+++ src/Loader/Loader.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import CircularProgress from '@mui/material/CircularProgress';
+import { SxProps } from '@mui/material';
 
 export interface LoaderProps {
   color?: string;
+  sx?: SxProps;
   className?: string;
 }
 
-export const Loader: React.FC<LoaderProps> = ({ className }) => (
+export const Loader: React.FC<LoaderProps> = ({ className, sx }) => (
   <div className={className}>
-    <CircularProgress color='primary' />
+    <CircularProgress sx={sx} color='primary' />
   </div>
 );
 
