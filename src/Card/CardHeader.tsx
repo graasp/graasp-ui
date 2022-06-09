cd3a085ac77782d57a8ec8c711aeee877d46690e
spaenleh <basile.1358@gmail.com>
2022-06-09 14:17:29 +0200

    feat: replace sx by styled for CardHeader

--- src/Card/CardHeader.tsx
+++ src/Card/CardHeader.tsx
@@ -1,6 +1,7 @@
 import React, { FC, ReactElement } from 'react';
 import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Grid';
+import { styled } from '@mui/material';
 
 type CardHeaderProps = {
   name: string;
@@ -9,6 +10,33 @@ type CardHeaderProps = {
   NameWrapper?: FC;
 };
 
+const StyledGridContainer = styled(Grid)(({ theme }) => ({
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+  paddingLeft: theme.spacing(1),
+  width: '100%',
+}));
+
+const ItemMenuContainer = styled(Grid)({
+  textAlign: 'right',
+});
+
+const StyledName = styled(Typography)({
+  fontSize: '0.9rem',
+  fontWeight: 'bold',
+  overflow: 'hidden',
+  textOverflow: 'ellipsis',
+  whiteSpace: 'nowrap',
+});
+
+const CreatorTypography = styled(Typography)({
+  fontSize: '0.72rem',
+  overflow: 'hidden',
+  textOverflow: 'ellipsis',
+  whiteSpace: 'nowrap',
+});
+
 const CustomCardHeader: FC<CardHeaderProps> = ({
   name,
   creator,
@@ -16,53 +44,25 @@ const CustomCardHeader: FC<CardHeaderProps> = ({
   NameWrapper = ({ children }: { children: ReactElement }) => children,
 }) => {
   return (
-    <Grid
-      container
-      sx={{
-        display: 'flex',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        paddingLeft: 1,
-        width: '100%',
-      }}
-    >
+    <StyledGridContainer container>
       <Grid item xs={9} sx={{ display: 'flex', width: '100%' }}>
         <Grid container>
           <Grid item xs={12}>
             <NameWrapper>
-              <Typography
-                sx={{
-                  fontSize: '0.9rem',
-                  fontWeight: 'bold',
-                  overflow: 'hidden',
-                  textOverflow: 'ellipsis',
-                  whiteSpace: 'nowrap',
-                }}
-              >
-                {name}
-              </Typography>
+              <StyledName>{name}</StyledName>
             </NameWrapper>
           </Grid>
           {creator && (
             <Grid item xs={12}>
-              <Typography
-                sx={{
-                  fontSize: '0.72rem',
-                  overflow: 'hidden',
-                  textOverflow: 'ellipsis',
-                  whiteSpace: 'nowrap',
-                }}
-              >
-                {creator}
-              </Typography>
+              <CreatorTypography>{creator}</CreatorTypography>
             </Grid>
           )}
         </Grid>
       </Grid>
-      <Grid item xs={3} sx={{ textAlign: 'right' }}>
+      <ItemMenuContainer item xs={3}>
         {ItemMenu}
-      </Grid>
-    </Grid>
+      </ItemMenuContainer>
+    </StyledGridContainer>
   );
 };
 
