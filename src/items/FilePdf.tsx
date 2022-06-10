f92b5e0ac4f7542de1ae315a98ff3b9fd5c44e6c
alvrba <alvaro.b72@gmail.com>
2022-06-10 18:35:57 +0200

    fix: set height depending on showCollapse

--- src/items/FilePdf.tsx
+++ src/items/FilePdf.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useRef } from 'react';
+import React, { FC, useRef, useState, ReactEventHandler } from 'react';
 import clsx from 'clsx';
 import { makeStyles } from '@material-ui/core/styles';
 import { ITEM_MAX_HEIGHT } from '../constants';
@@ -8,6 +8,7 @@ interface FilePdfProps {
   url: string;
   height?: number | string;
   className?: string;
+  showCollapse?: boolean;
 }
 
 const useStyles = makeStyles({
@@ -21,9 +22,24 @@ const FilePdf: FC<FilePdfProps> = ({
   id,
   height: defaultHeight,
   className,
+  showCollapse,
 }) => {
   const embedRef = useRef<HTMLEmbedElement>(null);
   const classes = useStyles();
+  const [height, setHeight] = useState<number | string>(
+    defaultHeight ?? '100%',
+  );
+
+  const onLoad: ReactEventHandler<HTMLEmbedElement> = (e) => {
+    // only set pdf height if not using collapse
+    if (!showCollapse) {
+      // set pdf height -> probably very high
+      console.log((e.target as HTMLEmbedElement)?.offsetParent);
+      const newHeight = (e.target as HTMLEmbedElement)?.offsetParent
+        ?.scrollHeight;
+      newHeight && setHeight(newHeight);
+    };
+  };
 
   return (
     <embed
@@ -31,7 +47,8 @@ const FilePdf: FC<FilePdfProps> = ({
       id={id}
       src={url}
       width='100%'
-      height={defaultHeight || ITEM_MAX_HEIGHT}
+      height={height || '100%'}
+      onLoad={onLoad}
       className={clsx(classes.embed, className)}
     />
   );
