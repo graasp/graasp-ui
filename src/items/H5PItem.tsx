c8bbe0ccada5803cb25ec8db295b938166b53870
Alexandre Chau <accounts@chau.moe>
2022-08-08 16:03:37 +0200

    refactor: use external integration iframe

--- src/items/H5PItem.tsx
+++ src/items/H5PItem.tsx
@@ -1,20 +1,12 @@
-import { H5POptions } from 'h5p-standalone';
 import React, { FC } from 'react';
-
-/**
- * Helper to generate unique H5P container IDs
- * (multiple H5Ps can be displayed simultaneously)
- */
-export const buildH5PContainerId = (itemId: string) =>
-  `h5p-container-${itemId}`;
 
 /**
  * React props types for {@link H5PItem}
  */
 interface H5PItemProps {
   itemId: string;
-  h5pAssetsHost: string;
-  playerOptions: H5POptions;
+  contentId: string;
+  integrationUrl: string;
 }
 /**
  * The H5PItem component displays an iframe with the content of an H5P
@@ -22,11 +14,7 @@ interface H5PItemProps {
  * This component bridges the gap between the procedural "h5p-standalone"
  * package and the Graasp React ecosystem
  */
-const H5PItem: FC<H5PItemProps> = ({
-  itemId,
-  h5pAssetsHost,
-  playerOptions,
-}) => {
+const H5PItem: FC<H5PItemProps> = ({ itemId, contentId, integrationUrl }) => {
   /*
     h5p-standalone (and H5P itself) expect the integration to be done on the
     window global object, which does not allow multiple H5Ps to be loaded
@@ -35,30 +23,12 @@ const H5PItem: FC<H5PItemProps> = ({
     gets its own window object. We can also enable the sandbox attribute for
     additional security
    */
-  const containerId = buildH5PContainerId(itemId);
-  const container = `<div id="${containerId}"></div>`;
-  const script = `<script type="text/javascript" src="${h5pAssetsHost}/main.bundle.js"></script>`;
-  const init = `
-    <script type="text/javascript">
-      const el = document.getElementById("${containerId}");
-      const options = JSON.parse('${JSON.stringify(playerOptions)}');
-      new H5PStandalone.H5P(el, options);
-      const resizeObserver = new ResizeObserver(entries => {
-        for (let entry of entries) {
-          window.frameElement.height = entry.contentRect.height;
-        }
-      });
-      resizeObserver.observe(el);
-    </script>
-  `;
   return (
     <iframe
-      srcDoc={container + script + init}
+      id={`h5p-container-${itemId}`}
+      src={`${integrationUrl}?content=${encodeURIComponent(contentId)}`}
       scrolling={'no'}
       frameBorder={0}
-      sandbox={
-        'allow-forms allow-modals allow-pointer-lock allow-popups allow-presentation allow-scripts'
-      }
       style={{ width: '100%', border: 'none', display: 'block' }}
     ></iframe>
   );
