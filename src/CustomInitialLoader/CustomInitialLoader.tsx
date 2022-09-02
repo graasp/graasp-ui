814d2077843749a0b018e4306cbc6e1df315cfaf
Alvaro Bautista <67112345+alvrba@users.noreply.github.com>
2022-09-02 18:59:41 +0200

    refactor: include progress bar in CustomInitialLoader

--- src/CustomInitialLoader/CustomInitialLoader.tsx
+++ src/CustomInitialLoader/CustomInitialLoader.tsx
@@ -1,43 +1,34 @@
 import React from 'react';
-import { Typography, Container, makeStyles } from '@material-ui/core';
+import { Container, makeStyles, LinearProgress } from '@material-ui/core';
 import GraaspLogo from '../GraaspLogo';
 
 interface Props {
   id?: string;
-  text?: string;
 }
 
 const useStyles = makeStyles((theme) => ({
   container: {
     height: '100vh',
     display: 'flex',
+    flexDirection: 'column',
     justifyContent: 'center',
     alignItems: 'center',
   },
-  text: {
-    marginLeft: theme.spacing(2),
+  progressBar: {
+    marginTop: theme.spacing(2),
     color: theme.palette.primary.main,
   },
   logo: {
     fill: theme.palette.primary.main,
   },
-  link: {
-    textDecoration: 'none',
-    fontStyle: 'italic',
-    color: 'black',
-  },
 }));
 
-const CustomInitialLoader: React.FC<Props> = ({ text, id }) => {
+const CustomInitialLoader: React.FC<Props> = ({ id }) => {
   const classes = useStyles();
   return (
     <Container id={id} className={classes.container}>
       <GraaspLogo height={100} className={classes.logo} />
-      <div className={classes.text}>
-        <Typography variant='h4' align='center'>
-          {text ?? 'Loading…'}
-        </Typography>
-      </div>
+      <LinearProgress className={classes.progressBar} />
     </Container>
   );
 };
