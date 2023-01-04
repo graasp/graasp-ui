6fd3c3875647c051aa16411374b30de5cc8e4b60
Alexandre Chau <accounts@chau.moe>
2023-01-04 17:50:24 +0100

    feat: add etherpad iframe, improve H5P security

---
+++ src/items/EtherpadItem.tsx
@@ -0,0 +1,51 @@
+import React, { FC } from 'react';
+
+/**
+ * @see https://etherpad.org/doc/v1.8.18/#index_embed-parameters
+ */
+type EtherpadEmbedOptions = {
+  showLineNumbers?: boolean;
+  showControls?: boolean;
+  showChat?: boolean;
+  useMonospaceFont?: boolean;
+  userName?: string;
+  userColor?: string;
+  noColors?: boolean;
+  alwaysShowChat?: boolean;
+  lang?: string;
+  rtl?: boolean;
+  '#L'?: number;
+};
+
+interface EtherpadItemProps {
+  itemId: string;
+  padUrl: string;
+  iframeId?: string;
+  options?: EtherpadEmbedOptions;
+}
+
+const EtherpadItem: FC<EtherpadItemProps> = ({
+  itemId,
+  padUrl,
+  iframeId = `etherpad-container-${itemId}`,
+  options,
+}) => {
+  const src = new URL(padUrl);
+  if (options) {
+    Object.entries(options).forEach(([param, value]) =>
+      src.searchParams.set(param, String(value)),
+    );
+  }
+
+  return (
+    <iframe
+      id={iframeId}
+      src={src.href}
+      frameBorder={0}
+      sandbox='allow-forms allow-modals allow-orientation-lock allow-pointer-lock allow-popups allow-presentation allow-scripts'
+      style={{ width: '100%', border: 'none', display: 'block' }}
+    ></iframe>
+  );
+};
+
+export default EtherpadItem;
