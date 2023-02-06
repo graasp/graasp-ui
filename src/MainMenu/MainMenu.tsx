bb0ffd21c4b8c20769a7cf476022cc372964ba4e
pyphilia <pyphilia@gmail.com>
2023-02-06 15:24:25 +0100

    feat: add fullHeight prop to MainMenu

--- src/MainMenu/MainMenu.tsx
+++ src/MainMenu/MainMenu.tsx
@@ -1,14 +1,25 @@
 import List from '@mui/material/List';
 
-import React, { FC } from 'react';
+import React from 'react';
 
 export interface MainMenuProps {
   id?: string;
   children?: React.ReactElement | React.ReactElement[];
+  /** whether container is full height
+   * this allows possible footer elements */
+  fullHeight?: boolean;
 }
 
-export const MainMenu: FC<MainMenuProps> = ({ id, children }) => {
-  return <List id={id}>{children}</List>;
+export const MainMenu = ({
+  id,
+  children,
+  fullHeight,
+}: MainMenuProps): JSX.Element => {
+  return (
+    <List id={id} sx={{ height: fullHeight ? '100%' : undefined }}>
+      {children}
+    </List>
+  );
 };
 
 export default MainMenu;
