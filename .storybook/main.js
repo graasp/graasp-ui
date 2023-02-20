1347609d54b36ed542baf3fd4faa18eda5b8fe4c
MalinSvenberg <56155987+MalinSvenberg@users.noreply.github.com>
2023-02-20 15:32:09 +0100

    build: Adapt Storybook for node18 (#244)
    * build: Adapt Storybook for node18
    
    * fix: Removes webpack4 dependencies

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,8 @@
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  core: {
+    builder: 'webpack5',
+  },
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
