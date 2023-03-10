98534c7826af9d4b030add1c249b60762258c2a8
Alexandre Chau <accounts@chau.moe>
2023-03-10 09:38:34 +0100

    feat: add svg padding, change colors

--- src/icons/EtherpadIcon.tsx
+++ src/icons/EtherpadIcon.tsx
@@ -12,7 +12,7 @@ const EtherpadIcon = ({ style, sx }: Props): JSX.Element => {
 
   return (
     <SvgIcon
-      viewBox='0 0 340 340'
+      viewBox='0 0 380 380'
       version='1.1'
       id='svg148'
       xmlnsXlink='http://www.w3.org/1999/xlink'
@@ -20,14 +20,8 @@ const EtherpadIcon = ({ style, sx }: Props): JSX.Element => {
       sx={sx}
     >
       <defs>
-        <rect id='path-2' x='42' y='167' width='168' height='27' rx='13.5' />
-        <filter
-          id='filter-3'
-          x='-.057143'
-          y='-.35556'
-          width='1.1143'
-          height='2.0074'
-        >
+        <rect id='c' x='42' y='167' width='168' height='27' rx='13.5' />
+        <filter id='f' x='-.0571' y='-.356' width='1.11' height='2.01'>
           <feOffset
             dx='0'
             dy='8'
@@ -44,14 +38,8 @@ const EtherpadIcon = ({ style, sx }: Props): JSX.Element => {
             values='0 0 0 0 0   0 0 0 0 0   0 0 0 0 0  0 0 0 0.0568181818 0'
           />
         </filter>
-        <rect id='path-4' x='41' y='110' width='142' height='25' rx='12.5' />
-        <filter
-          id='filter-5'
-          x='-.067606'
-          y='-.384'
-          width='1.1352'
-          height='2.088'
-        >
+        <rect id='b' x='41' y='110' width='142' height='25' rx='12.5' />
+        <filter id='e' x='-.0676' y='-.384' width='1.14' height='2.09'>
           <feOffset
             dx='0'
             dy='8'
@@ -68,14 +56,8 @@ const EtherpadIcon = ({ style, sx }: Props): JSX.Element => {
             values='0 0 0 0 0   0 0 0 0 0   0 0 0 0 0  0 0 0 0.0568181818 0'
           />
         </filter>
-        <rect id='path-6' x='41' y='226' width='105' height='25' rx='12.5' />
-        <filter
-          id='filter-7'
-          x='-.091429'
-          y='-.384'
-          width='1.1829'
-          height='2.088'
-        >
+        <rect id='a' x='41' y='226' width='105' height='25' rx='12.5' />
+        <filter id='d' x='-.0914' y='-.384' width='1.18' height='2.09'>
           <feOffset
             dx='0'
             dy='8'
@@ -93,31 +75,67 @@ const EtherpadIcon = ({ style, sx }: Props): JSX.Element => {
           />
         </filter>
       </defs>
-      <g transform='translate(0,-15)' fill='none' fill-rule='evenodd'>
+      <g transform='translate(20,5)' fill='none' fill-rule='evenodd'>
         <g transform='translate(-415,-351)'>
           <g transform='translate(415,351)'>
             <g transform='translate(0,15)'>
               <rect width='340' height='340' rx='70' fill={color} />
               <path
-                d='m237.61 138.16c-2.8864-2.8757-7.561-2.878-10.447 0s-2.8864 7.5412 0 10.419c7.7751 7.7524 12.058 18.025 12.058 28.923s-4.2828 21.17-12.058 28.925c-2.8864 2.8757-2.8864 7.5412 0 10.417 1.4434 1.4388 3.3324 2.1584 5.2237 2.1584 1.889 0 3.7802-0.71962 5.2237-2.1584 10.568-10.537 16.388-24.507 16.388-39.342 0-14.834-5.8199-28.805-16.388-39.342z'
-                fill='#000'
-                fill-opacity='.20048'
+                d='m238 138c-2.89-2.88-7.56-2.88-10.4 0s-2.89 7.54 0 10.4c7.78 7.75 12.1 18 12.1 28.9s-4.28 21.2-12.1 28.9c-2.89 2.88-2.89 7.54 0 10.4 1.44 1.44 3.33 2.16 5.22 2.16 1.89 0 3.78-0.72 5.22-2.16 10.6-10.5 16.4-24.5 16.4-39.3 0-14.8-5.82-28.8-16.4-39.3z'
+                fill='#fff'
+                fill-opacity='1'
                 fill-rule='nonzero'
-                opacity='.75406'
+                opacity='.754'
               />
               <path
-                d='m267.33 113.16c-2.8225-2.8782-7.3936-2.8782-10.216 0-2.8225 2.8805-2.8225 7.5504 0 10.428 28.587 29.176 28.587 76.651 0 105.83-2.8225 2.8782-2.8225 7.5477 0 10.426 1.4115 1.4405 3.2587 2.1603 5.1081 2.1603s3.6966-0.72024 5.1081-2.163c34.223-34.924 34.223-91.753 0-126.68z'
-                fill='#131514'
-                fill-opacity='.25056'
+                d='m267 113c-2.82-2.88-7.39-2.88-10.2 0-2.82 2.88-2.82 7.55 0 10.4 28.6 29.2 28.6 76.7 0 106-2.82 2.88-2.82 7.55 0 10.4 1.41 1.44 3.26 2.16 5.11 2.16s3.7-0.72 5.11-2.16c34.2-34.9 34.2-91.8 0-127z'
+                fill='#fff'
+                fill-opacity='1'
                 fill-rule='nonzero'
-                opacity='.75406'
+                opacity='.754'
               />
-              <use fill='#000000' filter='url(#filter-3)' xlinkHref='#path-2' />
-              <use fill='#ffffff' fill-rule='evenodd' xlinkHref='#path-2' />
-              <use fill='#000000' filter='url(#filter-5)' xlinkHref='#path-4' />
-              <use fill='#ffffff' fill-rule='evenodd' xlinkHref='#path-4' />
-              <use fill='#000000' filter='url(#filter-7)' xlinkHref='#path-6' />
-              <use fill='#ffffff' fill-rule='evenodd' xlinkHref='#path-6' />
+              <use
+                width='100%'
+                height='100%'
+                fill='#000000'
+                filter='url(#f)'
+                xlinkHref='#c'
+              />
+              <use
+                width='100%'
+                height='100%'
+                fill='#ffffff'
+                fill-rule='evenodd'
+                xlinkHref='#c'
+              />
+              <use
+                width='100%'
+                height='100%'
+                fill='#000000'
+                filter='url(#e)'
+                xlinkHref='#b'
+              />
+              <use
+                width='100%'
+                height='100%'
+                fill='#ffffff'
+                fill-rule='evenodd'
+                xlinkHref='#b'
+              />
+              <use
+                width='100%'
+                height='100%'
+                fill='#000000'
+                filter='url(#d)'
+                xlinkHref='#a'
+              />
+              <use
+                width='100%'
+                height='100%'
+                fill='#ffffff'
+                fill-rule='evenodd'
+                xlinkHref='#a'
+              />
             </g>
           </g>
         </g>
