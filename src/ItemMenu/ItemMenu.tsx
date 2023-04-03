967b07e426a4f6c858655a9dc86d3d96eb61a341
Malin Svenberg <malin.svenberg@hotmail.com>
2023-04-03 22:23:31 +0200

    feat: adds ItemMenu

---
+++ src/ItemMenu/ItemMenu.tsx
@@ -0,0 +1,57 @@
+import Menu from '@mui/material/Menu';
+
+import { FC, useState } from 'react';
+import React from 'react';
+
+import VerticalMenuButton from '../buttons/VerticalMenuButton';
+
+type Props = {
+  menuButtonId?: string;
+  menuId?: string;
+  menuButtonClassName?: string;
+  menuClassName?: string;
+  children?: React.ReactElement | React.ReactElement[];
+  openMenuText?: string;
+};
+
+const ItemMenu: FC<Props> = ({
+  menuButtonId,
+  menuId,
+  menuButtonClassName,
+  children,
+  openMenuText,
+}) => {
+  const [anchorEl, setAnchorEl] = useState<Element | null>(null);
+
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>): void => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = (): void => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <>
+      <VerticalMenuButton
+        id={menuButtonId}
+        className={menuButtonClassName}
+        onClick={handleClick}
+        openText={openMenuText}
+      />
+      {Boolean(anchorEl) && (
+        <Menu
+          id={menuId}
+          anchorEl={anchorEl}
+          keepMounted
+          open={Boolean(anchorEl)}
+          onClose={handleClose}
+        >
+          {children}
+        </Menu>
+      )}
+    </>
+  );
+};
+
+export default ItemMenu;
