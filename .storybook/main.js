5b8b87400c7e3da66011eefb0ebdfffb632cdcf5
pyphilia <pyphilia@gmail.com>
2023-05-22 17:54:29 +0200

    refactor: upgrade storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,19 +1,16 @@
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  core: {
-    builder: 'webpack5',
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions', '@storybook/addon-docs', '@storybook/addon-a11y', '@storybook/addon-coverage'],
+  framework: {
+    name: '@storybook/react-webpack5',
+    options: {}
   },
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
-    '@storybook/addon-docs',
-    '@storybook/addon-a11y',
-    '@storybook/addon-coverage',
-  ],
-  framework: '@storybook/react',
   features: {
-    interactionsDebugger: true, // 👈 Enable playback controls
+    interactionsDebugger: true // 👈 Enable playback controls
   },
+
   staticDirs: ['./public'],
+  docs: {
+    autodocs: true
+  }
 };
