4a3b82bf6f3e2fc3db9500e66222ae5813065cf0
Malin Svenberg <malin.svenberg@hotmail.com>
2023-06-09 20:52:16 +0200

    fix: adds mock member of memberRecord type

--- src/utils/fixtures.ts
+++ src/utils/fixtures.ts
@@ -1,4 +1,5 @@
 import { MemberType } from '@graasp/sdk';
+import { MemberRecord } from '@graasp/sdk/frontend';
 
 export const MOCK_MEMBER = {
   id: 'id',
@@ -9,3 +10,13 @@ export const MOCK_MEMBER = {
   updatedAt: new Date(),
   type: MemberType.Individual,
 };
+
+export const MOCK_MEMBER_RECORD = {
+  id: 'id',
+  name: 'creator',
+  email: 'email',
+  extra: {},
+  createdAt: new Date(),
+  updatedAt: new Date(),
+  type: MemberType.Individual,
+} as MemberRecord;
