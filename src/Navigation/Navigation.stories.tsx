4b564f695adc59aa2b4b5d10e33d70ac540f5f60
pyphilia <pyphilia@gmail.com>
2023-06-21 14:13:44 +0200

    refactor: add storybook test for navigation

--- src/Navigation/Navigation.stories.tsx
+++ src/Navigation/Navigation.stories.tsx
@@ -1,11 +1,19 @@
+import { expect } from '@storybook/jest';
 import type { Meta, StoryObj } from '@storybook/react';
+import { userEvent, within } from '@storybook/testing-library';
 import { List } from 'immutable';
 
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
-import { ItemType, LocalFileItemType, MimeTypes, convertJs } from '@graasp/sdk';
-import { ItemRecord } from '@graasp/sdk/frontend';
+import {
+  FolderItemType,
+  ItemType,
+  LocalFileItemType,
+  MimeTypes,
+  convertJs,
+} from '@graasp/sdk';
+import { FolderItemTypeRecord, ItemRecord } from '@graasp/sdk/frontend';
 
 import { MOCK_MEMBER } from '../utils/fixtures';
 import HomeMenu from './HomeMenu';
@@ -59,13 +67,30 @@ const useChildren: ItemMenuProps['useChildren'] = (id) => {
   return { data: children } as any;
 };
 const buildToItemPath = (id: string): string => id;
+const dataTestId = 'NavigateNextIcon';
+const folder = convertJs({
+  id: 'folder-id',
+  name: 'folder',
+  extra: {
+    [ItemType.FOLDER]: {
+      childrenOrder: [],
+    },
+  },
+  type: ItemType.FOLDER,
+  description: 'my image description',
+  path: 'item-path',
+  settings: {},
+  creator: MOCK_MEMBER,
+  createdAt: new Date(),
+  updatedAt: new Date(),
+}) as FolderItemTypeRecord;
 
 const menu = [
   { name: 'Home', id: 'home', to: 'home' },
   { name: 'Shared Items', id: 'shared', to: 'shared' },
 ];
 
-export const Default: Story = {
+export const HomeRoot: Story = {
   args: {
     buildToItemPath,
     useChildren,
@@ -89,6 +114,102 @@ export const Default: Story = {
         </>
       );
     },
+  },
+
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+
+    // 2 x Home
+    expect(canvas.getAllByTestId(dataTestId)).toHaveLength(2);
+  },
+};
+
+export const FolderWithParents: Story = {
+  args: {
+    buildToItemPath,
+    useChildren,
+    item: folder,
+
+    renderRoot: () => {
+      return (
+        <>
+          <HomeMenu selected={menu[0]} elements={menu} />
+          <ItemMenu
+            itemId={item.id}
+            useChildren={() => {
+              return {
+                data: List([
+                  buildItem('Home item 1'),
+                  buildItem('Home item 2'),
+                ]),
+              } as any;
+            }}
+            buildToItemPath={buildToItemPath}
+          />
+        </>
+      );
+    },
     parents,
+  },
+
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+
+    // current item
+    expect(canvas.getByText(folder.name)).toBeInTheDocument();
+
+    // check parents
+    for (const p of parents.toJS()) {
+      const b = canvas.getByText(p!.name);
+      expect(b).toBeInTheDocument();
+    }
+
+    // 4 = 2 parents + 2 x Home + current item is a folder
+    expect(canvas.getAllByTestId(dataTestId)).toHaveLength(5);
+  },
+};
+
+export const FileWithParents: Story = {
+  args: {
+    buildToItemPath,
+    useChildren,
+    item,
+
+    renderRoot: () => {
+      return (
+        <>
+          <HomeMenu selected={menu[0]} elements={menu} />
+          <ItemMenu
+            itemId={item.id}
+            useChildren={() => {
+              return {
+                data: List([
+                  buildItem('Home item 1'),
+                  buildItem('Home item 2'),
+                ]),
+              } as any;
+            }}
+            buildToItemPath={buildToItemPath}
+          />
+        </>
+      );
+    },
+    parents,
+  },
+
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+
+    // current item
+    expect(canvas.getByText(item.name)).toBeInTheDocument();
+
+    // check parents
+    for (const p of parents.toJS()) {
+      const b = canvas.getByText(p!.name);
+      expect(b).toBeInTheDocument();
+    }
+
+    // 4 = 2 parents + 2 x Home
+    expect(canvas.getAllByTestId(dataTestId)).toHaveLength(4);
   },
 };
