a5402ac5a8701d1b5b3c8e25da903ea300e26ce1
Kim Lan Phan Hoang <pyphilia@gmail.com>
2023-08-23 11:24:06 +0200

    refactor: remove s3file component (#501)

--- src/items/index.ts
+++ src/items/index.ts
@@ -1,7 +1,5 @@
 export { default as FileItem } from './FileItem';
 
-export { default as S3FileItem } from './S3FileItem';
-
 export { default as DocumentItem } from './DocumentItem';
 
 export { default as LinkItem } from './LinkItem';
