0ca1a2cc81ad6e09fe71a8ff63343c73ff55a63a
LinaYahya <linaebe0@gmail.com>
2023-09-19 14:17:33 +0200

    build: add console.error for getUUID catch

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -43,6 +43,7 @@ export const getUUID = (shortenUUID: string = ''): string => {
     }
     throw new Error('uuid should have a value');
   } catch (err) {
+    console.error(err)
     return shortenUUID;
   }
 };
