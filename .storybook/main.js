777304de35040cd1a7495995d14db4e3f77ed623
spaenleh <spaenleh@gmail.com>
2023-09-28 08:19:28 +0200

    fix: tests

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,16 +1,23 @@
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions', '@storybook/addon-docs', '@storybook/addon-a11y', '@storybook/addon-coverage'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    '@storybook/addon-docs',
+    '@storybook/addon-a11y',
+    '@storybook/addon-coverage',
+  ],
   framework: {
     name: '@storybook/react-webpack5',
-    options: {}
+    options: {},
   },
   features: {
-    interactionsDebugger: true // 👈 Enable playback controls
+    interactionsDebugger: true, // 👈 Enable playback controls
   },
 
   staticDirs: ['./public'],
   docs: {
-    autodocs: true
-  }
+    autodocs: true,
+  },
 };
