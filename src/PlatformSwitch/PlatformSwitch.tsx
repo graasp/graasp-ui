dd4a591818226713a70f69e0b30e1c27b2a50884
LinaYahya <linaebe0@gmail.com>
2024-01-03 14:03:07 +0100

    fix: formatting

--- src/PlatformSwitch/PlatformSwitch.tsx
+++ src/PlatformSwitch/PlatformSwitch.tsx
@@ -182,7 +182,7 @@ export const PlatformSwitch: FC<PlatformSwitchProps> = ({
               size,
               secondaryColor: isSelectedPlatform ? accentColor : color,
               primaryColor: isSelectedPlatform ? color : accentColor,
-              fill: isSelectedPlatform? accentColor: color,
+              fill: isSelectedPlatform ? accentColor : color,
               // platform-specific styles should override existing ones
               sx: { ...sx, ...platformProps?.sx },
             };
