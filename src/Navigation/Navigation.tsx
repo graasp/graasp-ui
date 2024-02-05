1b6a105bc176187e3e74e287343c34a0f760abbb
LinaYahya <linaebe0@gmail.com>
2024-02-05 18:05:30 +0100

    fix: move render navigation parts to a seperate components

--- src/Navigation/Navigation.tsx
+++ src/Navigation/Navigation.tsx
@@ -1,16 +1,14 @@
-import truncate from 'lodash.truncate';
-
-import { Box, SvgIconTypeMap, SxProps } from '@mui/material';
+import { SxProps } from '@mui/material';
 import Breadcrumbs from '@mui/material/Breadcrumbs';
-import Typography from '@mui/material/Typography';
 import { styled } from '@mui/material/styles';
-import { OverridableComponent } from '@mui/types';
 
-import { DiscriminatedItem, ItemType } from '@graasp/sdk';
+import { DiscriminatedItem } from '@graasp/sdk';
 
-import ItemActionsMenu from './ExtraItemsMenu';
-import ItemMenu, { ItemMenuProps } from './ItemMenu';
-import { CenterAlignWrapper, ITEM_NAME_MAX_LENGTH, StyledLink } from './utils';
+import CurrentItemNavigation from './CurrentItemNavigation';
+import ExtraItemsNavigation, { ExtraItem } from './ExtraItemsNavigation';
+import { ItemMenuProps } from './ItemMenu';
+import ParentsNavigation from './ParentsNavigation';
+import { CenterAlignWrapper } from './utils';
 
 const StyledBreadcrumbs = styled(Breadcrumbs)(({ theme }) => ({
   '& ol': {
@@ -38,12 +36,7 @@ export interface MenuItemType {
   name: string;
   path: string;
 }
-export interface ExtraItem {
-  name: string;
-  path?: string;
-  Icon?: OverridableComponent<SvgIconTypeMap>;
-  menuItems?: MenuItemType[];
-}
+
 const Navigation = ({
   backgroundColor,
   buildBreadcrumbsItemLinkId,
@@ -60,87 +53,6 @@ const Navigation = ({
   maxItems = 4,
   extraItems,
 }: NavigationProps): JSX.Element | null => {
-  const renderParents = (): JSX.Element[] | undefined =>
-    // need to convert otherwise it returns List<Element>
-    parents?.map(({ name, id }) => (
-      <CenterAlignWrapper key={id}>
-        <StyledLink
-          id={buildBreadcrumbsItemLinkId?.(id)}
-          to={buildToItemPath(id)}
-        >
-          <Typography>
-            {truncate(name, { length: ITEM_NAME_MAX_LENGTH })}
-          </Typography>
-        </StyledLink>
-        <ItemMenu
-          useChildren={useChildren}
-          itemId={id}
-          buildToItemPath={buildToItemPath}
-        />
-      </CenterAlignWrapper>
-    ));
-
-  const renderCurrentItem = (): JSX.Element | null => {
-    if (!item) {
-      return null;
-    }
-
-    return (
-      <CenterAlignWrapper>
-        <StyledLink
-          id={buildBreadcrumbsItemLinkId?.(item.id)}
-          key={item.id}
-          to={buildToItemPath(item?.id)}
-        >
-          <Typography>
-            {truncate(item.name, { length: ITEM_NAME_MAX_LENGTH })}
-          </Typography>
-        </StyledLink>
-        {(item.type === ItemType.FOLDER || extraItems?.length) && (
-          <ItemMenu
-            useChildren={useChildren}
-            itemId={item.id}
-            buildToItemPath={buildToItemPath}
-            buildIconId={buildIconId}
-            buildMenuItemId={buildMenuItemId}
-            buildMenuId={buildMenuId}
-            renderArrow={Boolean(extraItems?.length)}
-          />
-        )}
-      </CenterAlignWrapper>
-    );
-  };
-
-  const renderExtraItems = (): JSX.Element[] | null => {
-    if (!extraItems) {
-      return null;
-    }
-
-    return extraItems.map(({ Icon, name, path, menuItems }) => (
-      <CenterAlignWrapper>
-        {/* margin set to -2 as menu list has a default style for text indent
-        with the same value, So to align menu items with this box menu item */}
-        <Box display='flex' gap={2} ml={-2}>
-          {Icon && <Icon />}
-          {path ? (
-            <StyledLink to={path}>
-              <Typography>
-                {truncate(name, { length: ITEM_NAME_MAX_LENGTH })}
-              </Typography>
-            </StyledLink>
-          ) : (
-            <Typography>
-              {truncate(name, { length: ITEM_NAME_MAX_LENGTH })}
-            </Typography>
-          )}
-        </Box>
-        {menuItems && menuItems.length > 0 && (
-          <ItemActionsMenu menuItems={menuItems} name={name} />
-        )}
-      </CenterAlignWrapper>
-    ));
-  };
-
   return (
     <StyledBreadcrumbs
       sx={sx}
@@ -151,9 +63,27 @@ const Navigation = ({
       style={{ backgroundColor }}
     >
       <CenterAlignWrapper>{renderRoot?.(item)}</CenterAlignWrapper>
-      {item?.id && renderParents()}
-      {item?.id && renderCurrentItem()}
-      {renderExtraItems()}
+      {item?.id && parents && (
+        <ParentsNavigation
+          useChildren={useChildren}
+          parents={parents}
+          buildToItemPath={buildToItemPath}
+          buildBreadcrumbsItemLinkId={buildBreadcrumbsItemLinkId}
+        />
+      )}
+      {item?.id && item && (
+        <CurrentItemNavigation
+          item={item}
+          useChildren={useChildren}
+          buildToItemPath={buildToItemPath}
+          buildBreadcrumbsItemLinkId={buildBreadcrumbsItemLinkId}
+          buildIconId={buildIconId}
+          buildMenuId={buildMenuId}
+          buildMenuItemId={buildMenuItemId}
+          showArrow={Boolean(extraItems?.length)}
+        />
+      )}
+      {extraItems && <ExtraItemsNavigation extraItems={extraItems} />}
     </StyledBreadcrumbs>
   );
 };
