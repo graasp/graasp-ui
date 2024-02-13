ef469ff04b621bf84bb0d5ea5122191a9d039eae
spaenleh <spaenleh@gmail.com>
2024-02-13 11:31:59 +0100

    fix: forbidden text display

--- src/itemLogin/ForbiddenContent.stories.tsx
+++ src/itemLogin/ForbiddenContent.stories.tsx
@@ -22,6 +22,15 @@ type Story = StoryObj<typeof meta>;
 export const SignedIn = {
   args: {
     memberId: 'member-id',
+    authenticatedText: 'Ask the admin of the item to give you access',
+  },
+} satisfies Story;
+
+export const LongHelperText = {
+  args: {
+    memberId: 'member-id',
+    authenticatedText:
+      'As it turns out, this item is not willign to let you in. How bad... Mhmmm aybe you could ask a friend or the admin of the item to kindly let you access it. Do not forget to bring candy, as this should help you be accepted.',
   },
 } satisfies Story;
 
--- src/itemLogin/ForbiddenText.tsx
+++ src/itemLogin/ForbiddenText.tsx
@@ -1,40 +1,36 @@
 import BlockIcon from '@mui/icons-material/Block';
 import { Stack, Typography } from '@mui/material';
 
-import { FC } from 'react';
-
 import { UUID } from '@graasp/sdk';
 
 import { FORBIDDEN_TEXT } from './constants';
 
-export interface ForbiddenTextProps {
+export type ForbiddenTextProps = {
   id?: UUID;
   title?: string;
   helperText?: string;
-}
+};
 
-const ForbiddenText: FC<ForbiddenTextProps> = ({
+const ForbiddenText = ({
   id,
   title = FORBIDDEN_TEXT,
   helperText,
-}) => {
-  return (
-    <Stack
-      id={id}
-      direction='row'
-      alignItems='flex-start'
-      justifyContent='center'
-      spacing={1}
-    >
-      <BlockIcon
-        sx={{ alignSelf: 'stretch', height: '100%', aspectRatio: 1 }}
-      />
-      <Stack direction='column'>
-        <Typography variant='h4'>{title}</Typography>
-        <Typography color='text.secondary'>{helperText}</Typography>
-      </Stack>
+}: ForbiddenTextProps): JSX.Element => (
+  <Stack
+    id={id}
+    direction='row'
+    alignItems='center'
+    justifyContent='center'
+    spacing={2}
+  >
+    <BlockIcon sx={{ fontSize: '4em' }} />
+    <Stack direction='column'>
+      <Typography variant='h4'>{title}</Typography>
+      <Typography color='text.secondary' maxWidth='50ch'>
+        {helperText}
+      </Typography>
     </Stack>
-  );
-};
+  </Stack>
+);
 
 export default ForbiddenText;
