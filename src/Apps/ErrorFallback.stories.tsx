4cfbc9b88025558a9b08f36b89a9598486294289
Jérémy La Scala <jeremy.lascala@epfl.ch>
2024-03-13 10:16:08 +0100

    docs(apps): remove mui args in ErrorFallback

--- src/Apps/ErrorFallback.stories.tsx
+++ src/Apps/ErrorFallback.stories.tsx
@@ -1,26 +1,11 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
-import { TABLE_CATEGORIES } from '../utils/storybook';
 import ErrorFallback from './ErrorFallback';
 import { UserFeedback } from './types';
 
 const meta: Meta<typeof ErrorFallback> = {
   title: 'Apps/ErrorFallback',
   component: ErrorFallback,
-
-  tags: ['autodocs'],
-  argTypes: {
-    variant: {
-      table: {
-        category: TABLE_CATEGORIES.MUI,
-      },
-    },
-    sx: {
-      table: {
-        category: TABLE_CATEGORIES.MUI,
-      },
-    },
-  },
 };
 
 export default meta;
