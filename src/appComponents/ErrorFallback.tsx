d7b01f9c71734f0c77fb5cd01e4b2c0fa9e99565
Jérémy La Scala <jeremy.lascala@epfl.ch>
2024-03-13 11:41:49 +0100

    docs(apps): update example for ErrorFallback
    shows how to create ErrorBoundary using Sentry

--- src/appComponents/ErrorFallback.tsx
+++ src/appComponents/ErrorFallback.tsx
@@ -57,22 +57,37 @@ interface ErrorFallbackProps {
  * @returns A form to submit user feedback.
  * 
  * @example
- * <pre><code>
-const ErrorBoundary: FC<{ children?: ReactNode }> = ({ children }) => (
-  <Sentry.ErrorBoundary
-    // eslint-disable-next-line react/no-unstable-nested-components
-    fallback={({ error, componentStack, eventId }) => (
-      <ErrorFallback
-        error={error}
-        componentStack={componentStack}
-        eventId={eventId}
-      />
-    )}
-  >
-    {children}
-  </Sentry.ErrorBoundary>
-);
-</code></pre>
+const ErrorBoundary: FC<{ children?: ReactNode }> = ({ children }) => {
+  const { t: tFallback } = useTranslation('translations', {
+    keyPrefix: 'ERROR_BOUNDARY.FALLBACK',
+  });
+  return (
+    <Sentry.ErrorBoundary
+      // eslint-disable-next-line react/no-unstable-nested-components
+      fallback={({ error, componentStack, eventId }) => (
+        <ErrorFallback
+          error={error}
+          componentStack={componentStack}
+          eventId={eventId}
+          captureUserFeedback={Sentry.captureUserFeedback}
+          title={tFallback('MESSAGE_TITLE')}
+          formTitle={tFallback('MESSAGE_FEEDBACK')}
+          nameLabel={tFallback('NAME_LABEL')}
+          nameHelper={tFallback('NAME_HELPER')}
+          emailLabel={tFallback('EMAIL_LABEL')}
+          emailHelper={tFallback('EMAIL_HELPER')}
+          commentLabel={tFallback('COMMENT_LABEL')}
+          commentHelper={tFallback('COMMENT_HELPER')}
+          thanksMessage={tFallback('THANKS_FOR_FEEDBACK')}
+          sendButtonLabel={tFallback('SEND')}
+          errorDetailsLabel={tFallback('ERROR_DETAILS')}
+        />
+      )}
+    >
+      {children}
+    </Sentry.ErrorBoundary>
+  );
+};
  */
 const ErrorFallback: FC<ErrorFallbackProps> = ({
   error,
