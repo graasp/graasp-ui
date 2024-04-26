4461751beff42312bee7066f6f10bacf926f43ea
kim <kim.phanhoang@epfl.ch>
2024-04-26 11:13:07 +0200

    refactor: update type

--- src/Tree/types.ts
+++ src/Tree/types.ts
@@ -1,7 +1,7 @@
 export type NavigationElement = {
   id: string;
   name: string;
-  // important to have to compute if an element should be disabled
+  // important to compute if an element should be disabled
   path: string;
   icon?: JSX.Element;
 };
