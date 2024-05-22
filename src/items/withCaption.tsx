b91b70b38c7ef0a69483761a5d58e310cd9c9b96
spaenleh <spaenleh@gmail.com>
2024-05-22 11:34:52 +0200

    fix: add gap for description and show html only in iframe setting

--- src/items/withCaption.tsx
+++ src/items/withCaption.tsx
@@ -2,14 +2,40 @@ import TextDisplay from '@/TextDisplay/TextDisplay';
 
 import { Box, Stack } from '@mui/material';
 
-import { DescriptionPlacement, DescriptionPlacementType } from '@graasp/sdk';
+import {
+  DescriptionPlacement,
+  DescriptionPlacementType,
+  UnionOfConst,
+} from '@graasp/sdk';
 
 const DEFAULT_ITEM_DESCRIPTION = '';
+
+export const Alignment = {
+  Center: 'center',
+  Left: 'left',
+  Right: 'right',
+} as const;
+export type AlignmentType = UnionOfConst<typeof Alignment>;
+
+const getAlignItemsFromAlignmentSetting = (
+  alignment: AlignmentType,
+): 'flex-start' | 'flex-end' | 'center' => {
+  switch (alignment) {
+    case Alignment.Right:
+      return 'flex-end';
+    case Alignment.Center:
+      return 'center';
+    case Alignment.Left:
+    default:
+      return 'flex-start';
+  }
+};
 
 type WithCaptionItem = {
   description: string | null;
   settings?: {
     descriptionPlacement?: DescriptionPlacementType;
+    alignment?: AlignmentType;
   };
 };
 
@@ -22,12 +48,14 @@ function withCaption<T extends WithCaptionItem>({ item }: WithCaptionProps<T>) {
     const ChildComponent = (): JSX.Element => {
       const descriptionPlacement =
         item.settings?.descriptionPlacement ?? 'below';
+      const alignmentSetting = item.settings?.alignment ?? Alignment.Left;
       const direction =
         descriptionPlacement === DescriptionPlacement.ABOVE
           ? 'column-reverse'
           : 'column';
+      const alignItems = getAlignItemsFromAlignmentSetting(alignmentSetting);
       return (
-        <Stack direction={direction}>
+        <Stack direction={direction} gap={0.5} alignItems={alignItems}>
           {/* The box prevent the image to take full available space due to the stack */}
           <Box>{component}</Box>
           <TextDisplay content={item.description ?? DEFAULT_ITEM_DESCRIPTION} />
