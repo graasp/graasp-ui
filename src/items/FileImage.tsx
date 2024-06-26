9a8b297f09ef6d461690acf55e930a029887f177
spaenleh <spaenleh@gmail.com>
2024-06-26 14:00:41 +0200

    fix: allow image to display bigger than the original size

--- src/items/FileImage.tsx
+++ src/items/FileImage.tsx
@@ -6,7 +6,18 @@ export interface FileImageProps {
 
 const FileImage = ({ id, url, alt }: FileImageProps): JSX.Element => {
   return (
-    <img id={id} src={url} alt={alt} title={alt} style={{ maxWidth: '100%' }} />
+    <img
+      id={id}
+      src={url}
+      alt={alt}
+      title={alt}
+      style={{
+        // do not overflow the parent
+        maxWidth: '100%',
+        // display bigger than the original size of the parent want it
+        width: '100%',
+      }}
+    />
   );
 };
 
