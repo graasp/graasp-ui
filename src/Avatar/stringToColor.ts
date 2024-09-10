ae1069f3f382f3aeedc97ebec5221d68b9f09e16
spaenleh <spaenleh@gmail.com>
2024-09-10 14:07:14 +0200

    fix: add color grid

--- src/Avatar/stringToColor.ts
+++ src/Avatar/stringToColor.ts
@@ -1,23 +1,36 @@
+import { hslToRgb } from '@mui/material';
+
 /**
- * Generate a background color for avatars from the given string
+ * Compute the member unique color based on their id
+ * @param id memberId
+ * @returns a CSS color string that can be used to set for example the background of an avatar
+ */
+export const getColorFromId = (id: string): string => {
+  const rawHue = id.slice(0, 2);
+  const hue = (parseInt(rawHue, 16) / 256) * 360;
+  const rawSaturation = id[2];
+  const saturation = 65 + (16 - parseInt(rawSaturation, 16));
+  const newRgb = hslToRgb(`hsl(${hue}, ${saturation}, ${60})`);
+  return newRgb;
+};
+
+/**
+ * Generate an HTML color from a string.
+ * This can be used to generate stable avatar colors
  *
  */
 export const stringToColor = (name: string): string => {
   let hash = 0;
-  let i;
 
-  /* eslint-disable no-bitwise */
-  for (i = 0; i < name.length; i += 1) {
-    hash = name.charCodeAt(i) + ((hash << 5) - hash);
+  for (const char of name) {
+    hash = char.charCodeAt(0) + ((hash << 5) - hash);
   }
 
   let color = '#';
 
-  for (i = 0; i < 3; i += 1) {
+  for (let i = 0; i < 3; i += 1) {
     const value = (hash >> (i * 8)) & 0xff;
     color += `00${value.toString(16)}`.slice(-2);
   }
-  /* eslint-enable no-bitwise */
-
   return color;
 };
