054919f42e15d68c90156e1396d89e19ef20cf8b
spaenleh <spaenleh@gmail.com>
2024-11-27 13:25:15 +0100

    fix: cookie buttons

--- src/CookiesBanner/CookiesBanner.tsx
+++ src/CookiesBanner/CookiesBanner.tsx
@@ -1,4 +1,4 @@
-import { Button, Typography, styled } from '@mui/material';
+import { Box, Button, Typography, styled } from '@mui/material';
 
 import { CookieConsent } from 'react-cookie-consent';
 
@@ -6,12 +6,23 @@ import { CookieConsent } from 'react-cookie-consent';
 const DECLINE_BUTTON_ID = 'decline-button-id';
 
 // string classnames used to pass to CookieConsent
-const CONTAINER_CLASS_NAME = 'cookie-container-className';
-const BUTTON_CLASS_NAME = 'cookie-button-className';
+const CONTAINER_CLASS_NAME = 'cookie-container';
+const BUTTON_CLASS_NAME = 'cookie-button';
+const BUTTON_CONTAINER_CLASS_NAME = 'cookie-button-container';
 
-const StyledCookieConsent = styled(CookieConsent)(({ theme }) => ({
-  [CONTAINER_CLASS_NAME]: {
+const StyledCookieConsent = styled(Box)(({ theme }) => ({
+  [`& .${CONTAINER_CLASS_NAME}`]: {
     zIndex: `${(theme?.zIndex?.drawer ?? 0) + 1} !important`,
+    backgroundColor: 'rgb(53,53,53,15%) !important',
+    color: `${theme.palette.text.primary} !important`,
+  },
+
+  [`& .${BUTTON_CONTAINER_CLASS_NAME}`]: {
+    display: 'flex',
+    alignSelf: 'center',
+    color: 'red',
+    gap: theme.spacing(1),
+    paddingInlineEnd: theme.spacing(2),
   },
 }));
 
@@ -26,8 +37,7 @@ const CookieButton = (props: CookieButtonProps): JSX.Element => {
   return (
     <Button
       {...props}
-      sx={{ m: 1 }}
-      color={isDeclinedButton ? 'secondary' : 'primary'}
+      color={isDeclinedButton ? undefined : 'primary'}
       variant={isDeclinedButton ? 'outlined' : 'contained'}
     />
   );
@@ -50,20 +60,24 @@ const CookiesBanner = ({
   expires = 30,
   text = `We use cookies and other tracking technologies to improve your browsing experience on our website, to analyze our website traffic, and to understand where our visitors are coming from. By browsing our website, you consent to our use of cookies and other tracking technologies.`,
 }: CookiesBannerProps): JSX.Element => (
-  <StyledCookieConsent
-    buttonText={acceptText}
-    cookieName={cookieName}
-    expires={expires}
-    ButtonComponent={CookieButton}
-    declineButtonText={declineButtonText}
-    enableDeclineButton
-    containerClasses={CONTAINER_CLASS_NAME}
-    declineButtonId={DECLINE_BUTTON_ID}
-    disableButtonStyles
-    buttonClasses={BUTTON_CLASS_NAME}
-    extraCookieOptions={{ domain }}
-  >
-    <Typography variant='body1'>{text}</Typography>
+  <StyledCookieConsent>
+    <CookieConsent
+      buttonText={acceptText}
+      cookieName={cookieName}
+      expires={expires}
+      ButtonComponent={CookieButton}
+      declineButtonText={declineButtonText}
+      enableDeclineButton
+      containerClasses={CONTAINER_CLASS_NAME}
+      declineButtonId={DECLINE_BUTTON_ID}
+      // disableStyles
+      disableButtonStyles
+      buttonClasses={BUTTON_CLASS_NAME}
+      buttonWrapperClasses={BUTTON_CONTAINER_CLASS_NAME}
+      extraCookieOptions={{ domain }}
+    >
+      <Typography variant='note'>{text}</Typography>
+    </CookieConsent>
   </StyledCookieConsent>
 );
 
