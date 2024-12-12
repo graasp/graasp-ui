9d1b1bcd2cdc099c60f629bcf32a05f0c43a2b9e
Basile Spaenlehauer <spaenleh@gmail.com>
2024-12-12 12:24:35 +0100

    fix: removereact-reouter-dom on the link element (#1128)

--- src/items/LinkItem.tsx
+++ src/items/LinkItem.tsx
@@ -1,7 +1,6 @@
 import { Alert, Box, Link as MUILink, styled } from '@mui/material';
 
 import { Fragment, memo, useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
 
 import { LinkItemType, getLinkExtra } from '@graasp/sdk';
 
@@ -234,7 +233,7 @@ const LinkItem = ({
     }
 
     return (
-      <MUILink component={Link} to={url} onClick={onClick}>
+      <MUILink href={url} onClick={onClick}>
         {url}
       </MUILink>
     );
